ceef33c97090fa1076aac87a05fd7f11b3248d2a
Diego Perez <alphamander@gmail.com>
2016-11-11 15:43:49 -0600

    testing and new models

--- server/boot/auto_migrate.js
+++ server/boot/auto_migrate.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
-  app.dataSources.postgres.autoupdate(["act","dictum","user","expedient","providence","role"], function(err) {
+  app.dataSources.postgres.automigrate(["act","dictum","user","expedient","providence","role"], function(err) {
     if (err) throw err;
 
       console.log('Migrated');
---
+++ test/basicTest_spec.js
@@ -0,0 +1,17 @@
+var frisby = require('frisby');
+frisby.create('TEST ACT GET')
+  .get('http://localhost:3000/api/acts')
+  .expectStatus(200)
+.toss();
+frisby.create('TEST DICTUM GET')
+  .get('http://localhost:3000/api/dicta')
+  .expectStatus(200)
+.toss();
+frisby.create('TEST PROVIDENCE GET')
+  .get('http://localhost:3000/api/providences')
+  .expectStatus(200)
+.toss();
+frisby.create('TEST EXPEDIENT GET')
+  .get('http://localhost:3000/api/expedients')
+  .expectStatus(200)
+.toss();
