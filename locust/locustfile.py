940749f20128b751ce75cb061aacea9ccfa59692
alphamander <alfacomander@yahoo.com>
2016-11-17 17:20:14 -0600

    Locust test with websiteUser

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -35,3 +35,8 @@ class SITPage(TaskSet):
 
 class UserBehavior(TaskSet):
     tasks = {SITPage:10}
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 5000
+    max_wait = 9000
