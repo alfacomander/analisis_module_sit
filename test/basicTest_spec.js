fcaea3ac2bff49af0353193b9a2d4f85997f83a1
alphamander <alfacomander@yahoo.com>
2016-11-19 19:08:08 -0600

    Test changed

--- test/basicTest_spec.js
+++ test/basicTest_spec.js
@@ -1,18 +1,18 @@
 
 var frisby = require('frisby');
 frisby.create('TEST ACT GET')
-  .get('http://localhost:3001/api/acts')
+  .get('http://localhost:3000/api/acts')
   .expectStatus(200)
 .toss();
 frisby.create('TEST DICTUM GET')
-  .get('http://localhost:3001/api/dicta')
+  .get('http://localhost:3000/api/dicta')
   .expectStatus(200)
 .toss();
 frisby.create('TEST PROVIDENCE GET')
-  .get('http://localhost:3001/api/providences')
+  .get('http://localhost:3000/api/providences')
   .expectStatus(200)
 .toss();
 frisby.create('TEST EXPEDIENT GET')
-  .get('http://localhost:3001/api/expedients')
+  .get('http://localhost:3000/api/expedients')
   .expectStatus(200)
 .toss();
